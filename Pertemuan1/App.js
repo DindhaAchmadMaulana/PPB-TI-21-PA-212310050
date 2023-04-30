0ee5be696840db987386360261656cccfc28e139
DindhaAchmadMaulana <114462755+DindhaAchmadMaulana@users.noreply.github.com>
2023-04-30 16:39:23 +0700

    Profil

--- Pertemuan1/App.js
+++ Pertemuan1/App.js
@@ -5,14 +5,8 @@ export default function App () {
   return (
   <View style={{flex: 1}}>
     <StatusBar barStyle={'light-content'} backgroundColor='##212121'> </StatusBar>
-    <View style={{alignItems: 'center', backgroundColor: '#white'}}>
-      <Image source={{uri: 'https://avatarfiles.alphacoders.com/120/120464.png'}}
      style={{width: 100, height: 100, borderRadius: 100 / 2, marginTop:50}} />
-      <Text style={{marginTop: 15, fontSize: 22, fontWeight: 'bold', padding: 10, color: 'black'}}>MUHAMAD SUBHAN RIZKI AFIAT</Text>
-  
-      <View style={{flex:1, flexDirection: "row", justifyContent:'space-around', alignItems:'center'}}>
-      <Image source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png' }}  style={{
-        width:40, height: 40, marginRight:10,
+      <Text style={{marginTop: 15, fontSize: 22, fontWeight: 'bold', padding: 10, color: 'black'}}>DINDHA ACHMAD MAULANA</Text>
       }} />
 
         <Image source={{uri: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'}} style={{
